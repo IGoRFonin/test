c786053812d1c79649fbffbb8915c47d5f6eb32b
IGoRFonin <ifonin@dalee.ru>
2018-07-23 08:27:35 +0300

    start

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+module.exports = {
+    module: {
+        rules: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.pug$/,
+                use: [
+                    "html-loader",
+                    "pug-html-loader"
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader"
+                ]
+            },
+            {
+                test: /\.(bmp|gif|jpe?g|png|svgz?|webp)$/,
+                loader: 'file-loader'
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/index.pug",
+            filename: "./index.html"
+        }),
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        })
+    ]
+};
